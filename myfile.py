3dd8f9b2b4e23f16f216038520500687dd13d612
austin <atm426@nyu.edu>
2022-11-02 13:28:07 -0400

    first commit

---
+++ myfile.py
@@ -0,0 +1,10 @@
+import sys
+
+def my_function(x: float, y: float) -> float:
+    ''' takes in two floats (x,y) and returns
+    their sum '''
+    return x + y
+
+sum = my_function( float(sys.argv[1]), float(sys.argv[2]) )
+print("\n")
+print(sum)
