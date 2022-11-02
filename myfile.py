f1d1c8d94280044b58d3836226311ae500b1c718
austin <atm426@nyu.edu>
2022-11-02 14:24:15 -0400

    fdakl;

--- myfile.py
+++ myfile.py
@@ -1,5 +1,6 @@
 import sys
 # this comment is useless
+# so is this one
 def my_function(x: float, y: float) -> float:
     ''' takes in two floats (x,y) and returns
     their sum '''
--- myfile.py~
+++ myfile.py~
@@ -1,5 +1,5 @@
 import sys
-
+# this comment is useless
 def my_function(x: float, y: float) -> float:
     ''' takes in two floats (x,y) and returns
     their sum '''
